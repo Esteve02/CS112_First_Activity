3471a08842076441e1cb144b4fc65de20f7dd477
Esteve02 <144907076+Esteve02@users.noreply.github.com>
2023-09-13 04:48:00 -0700

    Add files via upload

---
+++ BUTAD, JOHN ESTEVE BSCS-1E.py
@@ -0,0 +1,10 @@
+
+
+print("John Esteve Butad")
+print("Bachelor of Science in Computer Science")
+print("BSCS 1E")
+print("21 Years old")
+print("July 03/2002")
+print("Scions Canitoan Cagayan De Oro City")
+
+
